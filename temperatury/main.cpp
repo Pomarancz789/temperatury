d23d84fda10734da06d8c1112056b33e16a3de79
Pomarancz789 <granat-02krawcy@icloud.com>
2021-12-01 09:56:00 +0100

    Initial Commit

---
+++ temperatury/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  temperatury
+//
+//  Created by Jakub Wtorkiewicz on 01/12/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
